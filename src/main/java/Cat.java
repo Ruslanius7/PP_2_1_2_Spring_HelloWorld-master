e7bd566f33d3b86805308fe0e340b8b3efe9df3e
Ruslan Perepelitsa <ruslanperepelitsa77@gmail.com>
2024-11-21 22:03:12 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,30 @@
+public class Cat {
+    private String name;
+    private int age;
+    private String message;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
